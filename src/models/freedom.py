27fc63987f5cbcd070cc7968403d38f9937e4048
TBI805 <34084035+TBI805@users.noreply.github.com>
2024-10-14 14:32:26 +0800

    Add files via upload

---
+++ src/models/freedom.py
@@ -0,0 +1,374 @@
+# coding: utf-8
+# @email: enoche.chow@gmail.com
+r"""
+FREEDOM: A Tale of Two Graphs: Freezing and Denoising Graph Structures for Multimodal Recommendation
+# Update: 01/08/2022
+"""
+
+
+import os
+import numpy as np
+import scipy.sparse as sp
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from src.common.abstract_recommender import GeneralRecommender
+from src.common.loss import BPRLoss, EmbLoss, L2Loss
+from src.utils.utils import build_sim, compute_normalized_laplacian
+
+from mamba_ssm import Mamba
+
+
+class FREEDOM(GeneralRecommender):
+    def __init__(self, config, dataset):
+        super(FREEDOM, self).__init__(config, dataset)
+
+        self.embedding_dim = config['embedding_size']
+        self.feat_embed_dim = config['feat_embed_dim']
+        self.knn_k = config['knn_k']
+        self.lambda_coeff = config['lambda_coeff']
+        self.cf_model = config['cf_model']
+        self.n_layers = config['n_mm_layers']
+        self.n_ui_layers = config['n_ui_layers']
+        self.reg_weight = config['reg_weight']
+        self.build_item_graph = True
+        self.mm_image_weight = config['mm_image_weight']
+        self.dropout1 = config['dropout']
+        self.degree_ratio = config['degree_ratio']
+
+        self.n_nodes = self.n_users + self.n_items
+
+        # mamba config
+        self.hidden_size = config["hidden_size"]
+        self.num_layers = config["num_layers"]
+        # Hyperparameters for Mamba block
+        self.d_state = config["d_state"]
+        self.d_conv = config["d_conv"]
+        self.expand = config["expand"]
+        self.dropout_prob = config["dropout_prob"]
+
+        self.LayerNorm1 = nn.LayerNorm(self.hidden_size, eps=1e-12)
+
+        self.dropout = nn.Dropout(self.dropout_prob)
+        self.act = nn.ReLU()
+        self.mamba_layers = nn.ModuleList([
+            MambaLayer(
+                d_model=self.hidden_size,
+                d_state=self.d_state,
+                d_conv=self.d_conv,
+                expand=self.expand,
+                dropout=self.dropout_prob,
+                num_layers=self.num_layers,
+            ) for _ in range(self.num_layers)
+        ])
+
+        self.mamba_layers_r = nn.ModuleList([
+            MambaLayer(
+                d_model=self.hidden_size,
+                d_state=self.d_state,
+                d_conv=self.d_conv,
+                expand=self.expand,
+                dropout=self.dropout_prob,
+                num_layers=self.num_layers,
+            ) for _ in range(self.num_layers)
+        ])
+
+        # load dataset info
+        self.interaction_matrix = dataset.inter_matrix(form='coo').astype(np.float32)
+        self.norm_adj = self.get_norm_adj_mat().to(self.device)
+        self.masked_adj, self.mm_adj = None, None
+        self.edge_indices, self.edge_values = self.get_edge_info()
+        self.edge_indices, self.edge_values = self.edge_indices.to(self.device), self.edge_values.to(self.device)
+        self.edge_full_indices = torch.arange(self.edge_values.size(0)).to(self.device)
+
+        self.user_embedding = nn.Embedding(self.n_users, self.embedding_dim)
+        self.item_id_embedding = nn.Embedding(self.n_items, self.embedding_dim)
+        nn.init.xavier_uniform_(self.user_embedding.weight)
+        nn.init.xavier_uniform_(self.item_id_embedding.weight)
+
+        dataset_path = os.path.abspath(config['data_path'] + config['dataset'])
+        mm_adj_file = os.path.join(dataset_path, 'mm_adj_freedomdsp_{}_{}.pt'.format(self.knn_k, int(10*self.mm_image_weight)))
+
+        if self.v_feat is not None:
+            self.image_embedding = nn.Embedding.from_pretrained(self.v_feat, freeze=False)
+            self.image_trs = nn.Linear(self.v_feat.shape[1], self.feat_embed_dim)
+        if self.t_feat is not None:
+            self.text_embedding = nn.Embedding.from_pretrained(self.t_feat, freeze=False)
+            self.text_trs = nn.Linear(self.t_feat.shape[1], self.feat_embed_dim)
+
+        if os.path.exists(mm_adj_file):
+            self.mm_adj = torch.load(mm_adj_file)
+        else:
+            if self.v_feat is not None:
+                indices, image_adj = self.get_knn_adj_mat(self.image_embedding.weight.detach())
+                self.mm_adj = image_adj
+            if self.t_feat is not None:
+                indices, text_adj = self.get_knn_adj_mat(self.text_embedding.weight.detach())
+                self.mm_adj = text_adj
+            if self.v_feat is not None and self.t_feat is not None:
+                self.mm_adj = self.mm_image_weight * image_adj + (1.0 - self.mm_image_weight) * text_adj
+                del text_adj
+                del image_adj
+            torch.save(self.mm_adj, mm_adj_file)
+
+    def get_knn_adj_mat(self, mm_embeddings):
+        context_norm = mm_embeddings.div(torch.norm(mm_embeddings, p=2, dim=-1, keepdim=True))
+        sim = torch.mm(context_norm, context_norm.transpose(1, 0))
+        _, knn_ind = torch.topk(sim, self.knn_k, dim=-1)
+        adj_size = sim.size()
+        del sim
+        # construct sparse adj
+        indices0 = torch.arange(knn_ind.shape[0]).to(self.device)
+        indices0 = torch.unsqueeze(indices0, 1)
+        indices0 = indices0.expand(-1, self.knn_k)
+        indices = torch.stack((torch.flatten(indices0), torch.flatten(knn_ind)), 0)
+        # norm
+        return indices, self.compute_normalized_laplacian(indices, adj_size)
+
+    def compute_normalized_laplacian(self, indices, adj_size):
+        adj = torch.sparse.FloatTensor(indices, torch.ones_like(indices[0]), adj_size)
+        row_sum = 1e-7 + torch.sparse.sum(adj, -1).to_dense()
+        r_inv_sqrt = torch.pow(row_sum, -0.5)
+        rows_inv_sqrt = r_inv_sqrt[indices[0]]
+        cols_inv_sqrt = r_inv_sqrt[indices[1]]
+        values = rows_inv_sqrt * cols_inv_sqrt
+        return torch.sparse.FloatTensor(indices, values, adj_size)
+
+    def get_norm_adj_mat(self):
+        A = sp.dok_matrix((self.n_users + self.n_items,
+                           self.n_users + self.n_items), dtype=np.float32)
+        inter_M = self.interaction_matrix
+        inter_M_t = self.interaction_matrix.transpose()
+        data_dict = dict(zip(zip(inter_M.row, inter_M.col + self.n_users),
+                             [1] * inter_M.nnz))
+        data_dict.update(dict(zip(zip(inter_M_t.row + self.n_users, inter_M_t.col),
+                                  [1] * inter_M_t.nnz)))
+        A._update(data_dict)
+        # norm adj matrix
+        sumArr = (A > 0).sum(axis=1)
+        # add epsilon to avoid Devide by zero Warning
+        diag = np.array(sumArr.flatten())[0] + 1e-7
+        diag = np.power(diag, -0.5)
+        D = sp.diags(diag)
+        L = D * A * D
+        # covert norm_adj matrix to tensor
+        L = sp.coo_matrix(L)
+        row = L.row
+        col = L.col
+        i = torch.LongTensor(np.array([row, col]))
+        data = torch.FloatTensor(L.data)
+
+        return torch.sparse.FloatTensor(i, data, torch.Size((self.n_nodes, self.n_nodes)))
+
+    def pre_epoch_processing(self):
+        if self.dropout1 <= .0:
+            self.masked_adj = self.norm_adj
+            return
+        # degree-sensitive edge pruning
+        degree_len = int(self.edge_values.size(0) * (1. - self.dropout1))
+        degree_idx = torch.multinomial(self.edge_values, degree_len)
+        # random sample
+        keep_indices = self.edge_indices[:, degree_idx]
+        # norm values
+        keep_values = self._normalize_adj_m(keep_indices, torch.Size((self.n_users, self.n_items)))
+        all_values = torch.cat((keep_values, keep_values))
+        # update keep_indices to users/items+self.n_users
+        keep_indices[1] += self.n_users
+        all_indices = torch.cat((keep_indices, torch.flip(keep_indices, [0])), 1)
+        self.masked_adj = torch.sparse.FloatTensor(all_indices, all_values, self.norm_adj.shape).to(self.device)
+
+    def _normalize_adj_m(self, indices, adj_size):
+        adj = torch.sparse.FloatTensor(indices, torch.ones_like(indices[0]), adj_size)
+        row_sum = 1e-7 + torch.sparse.sum(adj, -1).to_dense()
+        col_sum = 1e-7 + torch.sparse.sum(adj.t(), -1).to_dense()
+        r_inv_sqrt = torch.pow(row_sum, -0.5)
+        rows_inv_sqrt = r_inv_sqrt[indices[0]]
+        c_inv_sqrt = torch.pow(col_sum, -0.5)
+        cols_inv_sqrt = c_inv_sqrt[indices[1]]
+        values = rows_inv_sqrt * cols_inv_sqrt
+        return values
+
+    def get_edge_info(self):
+        rows = torch.from_numpy(self.interaction_matrix.row)
+        cols = torch.from_numpy(self.interaction_matrix.col)
+        edges = torch.stack([rows, cols]).type(torch.LongTensor)
+        # edge normalized values
+        values = self._normalize_adj_m(edges, torch.Size((self.n_users, self.n_items)))
+        return edges, values
+
+    def forward(self, adj):
+        h = self.item_id_embedding.weight
+        for i in range(self.n_layers):
+            h = torch.sparse.mm(self.mm_adj, h)
+
+        ego_embeddings = torch.cat((self.user_embedding.weight, self.item_id_embedding.weight), dim=0)
+        all_embeddings = [ego_embeddings]
+        for i in range(self.n_ui_layers):
+            side_embeddings = torch.sparse.mm(adj, ego_embeddings)
+            ego_embeddings = side_embeddings
+            all_embeddings += [ego_embeddings]
+        all_embeddings = torch.stack(all_embeddings, dim=1)
+        all_embeddings = all_embeddings.mean(dim=1, keepdim=False)
+        u_g_embeddings, i_g_embeddings = torch.split(all_embeddings, [self.n_users, self.n_items], dim=0)
+        return u_g_embeddings, i_g_embeddings + h
+
+    def bpr_loss(self, users, pos_items, neg_items):
+        pos_scores = torch.sum(torch.mul(users, pos_items), dim=1)
+        neg_scores = torch.sum(torch.mul(users, neg_items), dim=1)
+
+        maxi = F.logsigmoid(pos_scores - neg_scores)
+        mf_loss = -torch.mean(maxi)
+
+        return mf_loss
+
+    def calculate_loss(self, interaction):
+        users = interaction[0]
+        pos_items = interaction[1]
+        neg_items = interaction[2]
+
+        ua_embeddings, ia_embeddings = self.forward(self.masked_adj)
+        self.build_item_graph = False
+
+        u_g_embeddings = ua_embeddings[users]
+        pos_i_g_embeddings = ia_embeddings[pos_items]
+        neg_i_g_embeddings = ia_embeddings[neg_items]
+
+        batch_mf_loss = self.bpr_loss(u_g_embeddings, pos_i_g_embeddings, neg_i_g_embeddings)
+        mf_v_loss, mf_t_loss = 0.0, 0.0
+        if self.t_feat is not None:
+
+            text_feats = self.text_trs(self.text_embedding.weight)
+
+            # MambaFree
+            text_feats = torch.unsqueeze(text_feats, 0)
+            for i in range(self.num_layers):
+                text_feats = self.mamba_layers[i](text_feats)
+            text_feats = torch.squeeze(text_feats, 0)
+
+            mf_t_loss = self.bpr_loss(ua_embeddings[users], text_feats[pos_items], text_feats[neg_items])
+        if self.v_feat is not None:
+
+            image_feats = self.image_trs(self.image_embedding.weight)
+
+            # MambaFree
+            image_feats = torch.unsqueeze(image_feats, 0)
+            for i in range(self.num_layers):
+                image_feats = self.mamba_layers[i](image_feats)
+            image_feats = torch.squeeze(image_feats, 0)
+
+            mf_v_loss = self.bpr_loss(ua_embeddings[users], image_feats[pos_items], image_feats[neg_items])
+        return batch_mf_loss + self.reg_weight * (mf_t_loss + mf_v_loss)
+
+    def full_sort_predict(self, interaction):
+        user = interaction[0]
+
+        restore_user_e, restore_item_e = self.forward(self.norm_adj)
+        u_embeddings = restore_user_e[user]
+
+        # dot with all item embedding to accelerate
+        scores = torch.matmul(u_embeddings, restore_item_e.transpose(0, 1))
+        return scores
+
+
+class MambaLayer(nn.Module):
+    def __init__(self, d_model, d_state, d_conv, expand, dropout, num_layers):
+        super().__init__()
+        self.num_layers = num_layers
+        self.mamba = Mamba(
+            # This module uses roughly 3 * expand * d_model^2 parameters
+            d_model=d_model,
+            d_state=d_state,
+            d_conv=d_conv,
+            expand=expand,
+        )
+        self.dropout = nn.Dropout(dropout)
+        self.LayerNorm1 = nn.LayerNorm(d_model, eps=1e-12)
+        self.LayerNorm2 = nn.LayerNorm(d_model, eps=1e-12)
+        # self.RMSNnorm = RMSNorm(d_model)
+        self.ffn = FeedForward(d_model=d_model, inner_size=d_model * 4, dropout=dropout)
+
+    def forward(self, input_tensor):
+        hidden_states = self.mamba(input_tensor)
+        if self.num_layers == 1:  # one Mamba layer without residual connection
+            # hidden_states = self.dropout(hidden_states)
+            hidden_states = self.dropout(hidden_states)
+        else:  # stacked Mamba layers with residual connections
+            # hidden_states = self.dropout(hidden_states)
+
+            hidden_states = self.LayerNorm1(self.dropout(hidden_states) + input_tensor)
+            hidden_states = self.LayerNorm2(self.dropout(hidden_states) + input_tensor)
+
+        # hidden_states = self.ffn(hidden_states)
+
+        return hidden_states
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, inner_size, dropout=0.2):
+        super().__init__()
+        self.w_1 = nn.Linear(d_model, inner_size)
+        self.w_2 = nn.Linear(inner_size, d_model)
+        self.activation = nn.SiLU()
+        self.dropout = nn.Dropout(dropout)
+        self.LayerNorm = nn.LayerNorm(d_model, eps=1e-12)
+        # self.RMSNnorm = RMSNorm(d_model)
+
+    def forward(self, input_tensor):
+        hidden_states = self.w_1(input_tensor)
+        hidden_states = self.activation(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+
+        hidden_states = self.w_2(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        # hidden_states = self.RMSNnorm(hidden_states + input_tensor)
+
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+
+        return hidden_states
+
+class RMSNorm(nn.Module):
+    def __init__(self, d_model, p=-1., eps=1e-8, bias=False, dropout=.1):
+
+        """
+            Root Mean Square Layer Normalization
+        :param d_model: model size
+        :param p: partial RMSNorm, valid value [0, 1], default -1.0 (disabled)
+        :param eps:  epsilon value, default 1e-8
+        :param bias: whether use bias term for RMSNorm, disabled by
+            default because RMSNorm doesn't enforce re-centering invariance.
+        """
+        super(RMSNorm, self).__init__()
+
+        self.eps = eps
+        self.d = d_model
+        self.p = p
+        self.bias = bias
+        # self.gated = nn.Sequential(nn.Linear(2 * d_model, 2 * d_model),
+        #                            nn.GLU())
+        # self.dropout = nn.Dropout(dropout)
+
+        self.scale = nn.Parameter(torch.ones(d_model))
+        self.register_parameter("scale", self.scale)
+
+        if self.bias:
+            self.offset = nn.Parameter(torch.zeros(d_model))
+            self.register_parameter("offset", self.offset)
+
+    def forward(self, x):
+        if self.p < 0. or self.p > 1.:
+            norm_x = x.norm(2, dim=-1, keepdim=True)
+            d_x = self.d
+        else:
+            partial_size = int(self.d * self.p)
+            partial_x, _ = torch.split(x, [partial_size, self.d - partial_size], dim=-1)
+
+            norm_x = partial_x.norm(2, dim=-1, keepdim=True)
+            d_x = partial_size
+
+        rms_x = norm_x * d_x ** (-1. / 2)
+        x_normed = x / (rms_x + self.eps)
+
+        if self.bias:
+            return self.scale * x_normed + self.offset
+
+        return self.scale * x_normed
